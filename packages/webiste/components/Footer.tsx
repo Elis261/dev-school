3be8ed6a51c15f8273e8be6909a659fafaac02ea
Ennio <enniolopes@gmail.com>
2020-09-30 15:01:20 -0300

    feat(packages/website): init theme config

--- packages/webiste/components/Footer.tsx
+++ packages/webiste/components/Footer.tsx
@@ -1,14 +1,39 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer
       sx={{
-        backgroundColor: 'primary',
+        backgroundColor: 'background',
         height: '100px',
+        textAlign: 'center',
+        fontSize: 1,
       }}
-    />
+    >
+      <hr
+        sx={{
+          width: '95%',
+          height: '0.8px',
+          borderWidth: 0,
+          color: 'muted',
+          backgroundColor: 'muted',
+        }}
+      />
+      Supported by &nbsp;
+      <a
+        href="https://www.triangulostecnologia.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Triângulos Tecnologia
+      </a>
+      &nbsp; and &nbsp;
+      <Link href="/contributors">
+        <a>Contributors.</a>
+      </Link>
+    </footer>
   );
 };
 
